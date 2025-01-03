8c9d5b82bac29254b7e032df3f249a4b19edc319
gurudeep <vgurudeep@gmail.com>
2025-01-04 01:47:59 +0530

    added data and updated the planet.model

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,39 @@
-const planets = [];
+// const planets = [];
 
-module.exports = planets;
+const { parse } = require("csv-parse");
+const fs = require("fs");
+const path = require("path");
+
+const HabitablePlanets = [];
+
+function isHabitable(Planet) {
+  return (
+    Planet["koi_disposition"] === "CONFIRMED" &&
+    Planet["koi_insol"] > 0.36 &&
+    Planet["koi_insol"] < 1.11 &&
+    Planet["koi_prad"] < 1.6
+  );
+}
+function isAllPlanetsLoaded() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, "..", "data", "kepler_data.csv"))
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitable(data)) HabitablePlanets.push(data);
+      })
+      .on("error", (err) => reject(err))
+      .on("end", () => {
+        resolve();
+        console.log(
+          `The number of habitable planets found : ${HabitablePlanets.length}`
+        );
+      });
+  });
+}
+
+module.exports = { isAllPlanetsLoaded, planets: HabitablePlanets };
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,15 @@
 const http = require("http");
 const app = require("./app");
+const { isAllPlanetsLoaded } = require("./models/planets.model");
 
 const PORT = 8000;
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}...`);
-});
+async function loadServer() {
+  await isAllPlanetsLoaded();
+  server.listen(PORT, () => {
+    console.log(`Server running on ${PORT}...`);
+  });
+}
+
+loadServer();
